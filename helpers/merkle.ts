8449749ac41a6165dd821918ef99f7fa80cdf334
carlomigueldy <carlomigueldy@gmail.com>
2021-12-11 19:26:16 +0800

    Refactor helpers

--- helpers/merkle.ts
+++ helpers/merkle.ts
@@ -1,17 +1,35 @@
+import { ADVISOR_WHITELISTED_USERS, WHITELISTED_USERS } from "./whitelist";
 import { MerkleTree } from "merkletreejs";
 import keccak256 from "keccak256";
-// eslint-disable-next-line node/no-missing-import
-import { list } from "./userWhitelist";
 
-const leaves = list.map((x) => keccak256(x));
-const tree = new MerkleTree(leaves, keccak256);
+const whitelistLeaves = WHITELISTED_USERS.map((x) => keccak256(x));
+const advisorLeaves = ADVISOR_WHITELISTED_USERS.map((x) => keccak256(x));
+const whitelistTree = new MerkleTree(whitelistLeaves, keccak256);
+const advisorTree = new MerkleTree(advisorLeaves, keccak256);
 
-const getRoot = () => {
-  return tree.getRoot().toString("hex");
+const getWhitelistMerkleRoot = () => {
+  return "0x" + whitelistTree.getRoot().toString("hex");
 };
 
-const getProof = (leaf) => {
-  return tree.getProof(leaf);
+const getWhitelistMerkleProof = (leaf: string) => {
+  return whitelistTree.getProof(leaf);
 };
 
-export { getRoot, getProof, tree };
+const getAdvisorMerkleRoot = () => {
+  return "0x" + advisorTree.getRoot().toString("hex");
+};
+
+const getAdvisorMerkleProof = (leaf: string) => {
+  return advisorTree.getProof(leaf);
+};
+
+export {
+  getWhitelistMerkleRoot,
+  getWhitelistMerkleProof,
+  getAdvisorMerkleRoot,
+  getAdvisorMerkleProof,
+  whitelistLeaves,
+  advisorLeaves,
+  whitelistTree,
+  advisorTree,
+};
