372e263f563e04945d56d98ab63a14c4c904b542
carlomigueldy <carlomigueldy@gmail.com>
2021-12-13 18:54:21 +0800

    Refactor tests and add test to modifier isSaleOngoing

--- test/sale.spec.ts
+++ test/sale.spec.ts
@@ -1,33 +1,21 @@
-import { BigNumber } from "@ethersproject/bignumber";
+import {
+  advisorMerkleRoot,
+  generateWhitelistMerkleProof,
+  whitelistMerkleRoot,
+  WHITELIST_LEAVES,
+} from "../helpers/merkle";
+import { toUnixTimestamp } from "../helpers/time";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { Sale } from "../typechain";
-import {
-  getAdvisorMerkleRoot,
-  getWhitelistMerkleRoot,
-} from "../helpers/merkle";
-
-function toUnixTimestamp(date: string): BigNumber {
-  return BigNumber.from(new Date(date).getTime() / 1000);
-}
 
 describe("Sale", () => {
   let contract: Sale;
-  const whitelistMerkleRoot = getWhitelistMerkleRoot();
-  const advisorMerkleRoot = getAdvisorMerkleRoot();
-  const startSaleBlockTimestamp = toUnixTimestamp("2021-12-31");
-  const stopSaleBlockTimestamp = toUnixTimestamp("2022-01-31");
+  let startSaleBlockTimestamp = toUnixTimestamp("2021-12-31");
+  let stopSaleBlockTimestamp = toUnixTimestamp("2022-01-31");
 
-  beforeEach(async () => {
+  async function deploy() {
     const SaleContract = await ethers.getContractFactory("Sale");
-
-    console.log("beforeEach", {
-      whitelistMerkleRoot,
-      advisorMerkleRoot,
-      startSaleBlockTimestamp,
-      stopSaleBlockTimestamp,
-    });
-
     contract = await SaleContract.deploy(
       whitelistMerkleRoot,
       advisorMerkleRoot,
@@ -35,30 +23,42 @@ describe("Sale", () => {
       stopSaleBlockTimestamp
     );
     await contract.deployed();
-  });
+  }
+
+  beforeEach(deploy);
 
   describe("property: mintPrice", () => {
-    it("should return 0.2 ether", async () => {
+    it("SHOULD return 0.2 ether, WHEN called", async () => {
       const mintPrice = await contract.mintPrice();
 
       expect(ethers.utils.formatEther(mintPrice)).to.be.equal("0.2");
     });
   });
 
-  describe("property: startSaleBlockTimestamp", () => {
-    /** @todo */
-  });
-
-  describe("property: stopSaleBlockTimestamp", () => {
-    /** @todo */
-  });
-
-  describe("property: startKeyToScrollSwap", () => {
-    /** @todo */
-  });
-
   describe("modifier: isSaleOngoing", () => {
-    /** @todo */
+    beforeEach(async () => {
+      startSaleBlockTimestamp = toUnixTimestamp("2020-12-31");
+      stopSaleBlockTimestamp = toUnixTimestamp("2021-01-31");
+      await deploy();
+    });
+
+    it(`SHOULD revert with "Sale is over", WHEN GIVEN a valid merkle proof AND the sale timeframe is from the past`, async () => {
+      const [_startSaleBlockTimestamp, _stopSaleBlockTimestamp] =
+        await Promise.all([
+          contract.startSaleBlockTimestamp(),
+          contract.stopSaleBlockTimestamp(),
+        ]);
+
+      const [leaf] = WHITELIST_LEAVES;
+      const proof = generateWhitelistMerkleProof(leaf, 0);
+
+      expect(_startSaleBlockTimestamp).to.be.equal(startSaleBlockTimestamp);
+      expect(_stopSaleBlockTimestamp).to.be.equal(stopSaleBlockTimestamp);
+      await expect(contract.buyKeyFromSale(proof)).to.be.reverted;
+      await expect(contract.buyKeyFromSale(proof)).to.be.revertedWith(
+        "Sale is over"
+      );
+    });
   });
 
   describe("modifier: hasSaleEnded", () => {
