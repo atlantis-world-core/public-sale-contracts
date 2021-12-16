8ea765f21258e5d8e486c94287879ed90d4adbd9
rachit2501 <rac.sri25@gmail.com>
2021-12-17 01:35:02 +0530

    interface bug fix

--- contracts/interface/IScroll.sol
+++ contracts/interface/IScroll.sol
@@ -3,8 +3,4 @@ pragma solidity ^0.8.10;
 
 interface IScroll {
   function mint(address, uint256) external;
-
-  function getUserTokenDetails(address, uint256)
-    external
-    returns (Scroll memory);
 }
