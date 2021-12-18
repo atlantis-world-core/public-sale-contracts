46ea07201bdaeba345ab8048f0b7f5241bc68c9a
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-12-18 21:50:33 +0800

    Add test but failing

--- test/scroll-contract.spec.ts
+++ test/scroll-contract.spec.ts
@@ -1,11 +1,13 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { Contract } from "ethers";
+import { Sale, ScrollContract } from "../typechain";
 import { testSetup } from "./utils";
-import { DeployContractsFunction, TestSetupArgs } from "./utils/types";
+import { DeployContractsFunction } from "./utils/types";
 
 describe("ScrollContract", () => {
-  let scrollContract: Contract;
+  // contracts
+  let scrollContract: ScrollContract;
+  let saleContract: Sale;
 
   // helper
   let deployContracts: DeployContractsFunction;
@@ -13,7 +15,7 @@ describe("ScrollContract", () => {
   // signers
   let owner: SignerWithAddress;
 
-  const setup = async (args?: TestSetupArgs) => {
+  const setup = async () => {
     const { owner: _owner, deployContracts: _deployContracts } =
       await testSetup();
 
@@ -21,15 +23,19 @@ describe("ScrollContract", () => {
     deployContracts = _deployContracts;
 
     // Sale contract deploy
-    const { scrollContract: _scrollContract } = await deployContracts();
+    const { saleContract: _saleContract, scrollContract: _scrollContract } =
+      await deployContracts();
 
-    scrollContract = _scrollContract;
+    saleContract = _saleContract;
+    scrollContract = _scrollContract as ScrollContract;
+
+    scrollContract = scrollContract.connect(owner);
   };
 
   beforeEach(async () => await setup());
 
   describe("scrollContract.signer.getAddress", () => {
-    // test simple enough just to check if the test setup works 
+    // test simple enough just to check if the test setup works
     it("SHOULD be the owner address, WHEN called", async () => {
       const deployerAddress = await scrollContract.signer.getAddress();
       const ownerAddress = owner.address;
@@ -47,14 +53,30 @@ describe("ScrollContract", () => {
   });
 
   describe("mint", () => {
-    // 
-  })
+    it("SHOULD not revert WHEN called", async () => {
+      const [saleContractSignerAddress, scrollContractSignerAddress] =
+        await Promise.all([
+          saleContract.signer.getAddress(),
+          scrollContract.signer.getAddress(),
+        ]);
+
+      console.log({
+        ownerAddress: owner.address,
+        saleContractSignerAddress,
+        scrollContractSignerAddress,
+      });
+
+      await expect(
+        scrollContract.connect(owner).mint(owner.address, 1)
+      ).to.be.revertedWith("Hey");
+    });
+  });
 
   describe("setTokenURI", () => {
-    // 
-  })
+    //
+  });
 
   describe("setRoyalties", () => {
-    // 
-  })
+    //
+  });
 });
