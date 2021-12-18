ddb757ef06fbeb88407587804c859ba34d8056c4
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-12-18 16:48:03 +0800

    Add visibility declaration, and version bump

--- contracts/lib/LibRoyaltiesV2.sol
+++ contracts/lib/LibRoyaltiesV2.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 /// @notice Please note that the interface hash used in the below code can change from previous commits.
 /// @notice As of 11/8/2021 the current interface hash is 0xcad96cca.
 /// @notice Be sure to check if it changes at: https://github.com/rarible/protocol-contracts/blob/master/royalties/contracts/LibRoyaltiesV2.sol
-
 library LibRoyaltiesV2 {
   /*
    * bytes4(keccak256('getRoyalties(LibAsset.AssetType)')) == 0xcad96cca
    */
-  bytes4 constant _INTERFACE_ID_ROYALTIES = 0xcad96cca;
+  bytes4 internal constant _INTERFACE_ID_ROYALTIES = 0xcad96cca;
 }
