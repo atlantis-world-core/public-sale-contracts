67f85f23726aa0b5c9be2a0a407c01462b849696
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-12-18 20:06:13 +0800

    Add simple unit test for Scroll contract

--- test/scroll-contract.spec.ts
+++ test/scroll-contract.spec.ts
@@ -1,98 +1,60 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { BigNumber } from "ethers";
-import { ethers } from "hardhat";
-import { useMerkleHelper } from "../helpers/merkle";
-import { toUnixTimestamp } from "../helpers/time";
-import { Sale } from "../typechain";
+import { expect } from "chai";
+import { Contract } from "ethers";
 import { testSetup } from "./utils";
 import { DeployContractsFunction, TestSetupArgs } from "./utils/types";
-import MerkleTree from "merkletreejs";
 
 describe("ScrollContract", () => {
-  const merkleHelper = useMerkleHelper();
-
-  // contracts
-  let saleContract: Sale;
+  let scrollContract: Contract;
 
   // helper
   let deployContracts: DeployContractsFunction;
 
-  // sale timestamps
-  let startSaleBlockTimestamp: BigNumber = toUnixTimestamp("2021-12-31");
-  let stopSaleBlockTimestamp: BigNumber = toUnixTimestamp("2022-01-31");
-
   // signers
-  let whitelistSigners: SignerWithAddress[];
-  let advisorSigners: SignerWithAddress[];
   let owner: SignerWithAddress;
-  let minter: SignerWithAddress;
-  let advisor: SignerWithAddress;
-
-  // leaves
-  let whitelistLeaves: string[];
-  let advisorLeaves: string[];
-
-  // merkle trees
-  let whitelistMerkleTree: MerkleTree;
-  let advisorMerkleTree: MerkleTree;
-
-  // merkle roots
-  let whitelistMerkleRoot: string;
-  let advisorMerkleRoot: string;
-
-  // merkle proofs
-  const validWhitelistProof = (_leaf?: string): string[] => {
-    const [, leaf] = whitelistLeaves;
-    const hash = ethers.utils.keccak256(_leaf ?? leaf);
-    const proof = merkleHelper.createMerkleProof(whitelistMerkleTree, hash);
-    return proof;
-  };
-  const validAdvisorProof = (_leaf?: string): string[] => {
-    const [leaf] = advisorLeaves;
-    const hash = ethers.utils.keccak256(_leaf ?? leaf);
-    const proof = merkleHelper.createMerkleProof(advisorMerkleTree, hash);
-    return proof;
-  };
-  const invalidMerkleProof = (): string[] => [];
-
-  // ether
-  const validMintPayment = ethers.utils.parseEther("0.2");
-  const invalidMintPayment = ethers.utils.parseEther("0.01");
 
   const setup = async (args?: TestSetupArgs) => {
-    const {
-      owner: _owner,
-      minter: _minter,
-      advisor: _advisor,
-      advisorLeaves: _advisorLeaves,
-      advisorMerkleRoot: _advisorMerkleRoot,
-      advisorMerkleTree: _advisorMerkleTree,
-      whitelistLeaves: _whitelistLeaves,
-      whitelistMerkleRoot: _whitelistMerkleRoot,
-      whitelistMerkleTree: _whitelistMerkleTree,
-      deployContracts: _deployContracts,
-    } = await testSetup();
+    const { owner: _owner, deployContracts: _deployContracts } =
+      await testSetup();
 
     owner = _owner;
-    minter = _minter;
-    advisor = _advisor;
-    advisorLeaves = _advisorLeaves;
-    advisorMerkleRoot = _advisorMerkleRoot;
-    advisorMerkleTree = _advisorMerkleTree;
-    whitelistLeaves = _whitelistLeaves;
-    whitelistMerkleRoot = _whitelistMerkleRoot;
-    whitelistMerkleTree = _whitelistMerkleTree;
     deployContracts = _deployContracts;
 
     // Sale contract deploy
-    const { saleContract: _saleContract } = await deployContracts(
-      args?.saleStart ?? startSaleBlockTimestamp,
-      args?.saleStop ?? stopSaleBlockTimestamp
-    );
+    const { scrollContract: _scrollContract } = await deployContracts();
 
-    // connect as a minter
-    saleContract = _saleContract.connect(minter);
+    scrollContract = _scrollContract;
   };
 
   beforeEach(async () => await setup());
+
+  describe("scrollContract.signer.getAddress", () => {
+    // test simple enough just to check if the test setup works 
+    it("SHOULD be the owner address, WHEN called", async () => {
+      const deployerAddress = await scrollContract.signer.getAddress();
+      const ownerAddress = owner.address;
+
+      console.log(
+        scrollContract.address,
+        "|",
+        deployerAddress,
+        "==",
+        ownerAddress
+      );
+
+      expect(deployerAddress).to.be.eq(ownerAddress);
+    });
+  });
+
+  describe("mint", () => {
+    // 
+  })
+
+  describe("setTokenURI", () => {
+    // 
+  })
+
+  describe("setRoyalties", () => {
+    // 
+  })
 });
