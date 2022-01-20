ab8c70a2bccbbdd2f4e175ccb51aedfc8fdade95
rachit2501 <rac.sri25@gmail.com>
2022-01-20 19:20:35 +0530

    minor changes to config

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,6 +11,42 @@ import ALPHA_SALE_WHITELIST from "../helpers/alpha-sale-whitelist.json";
 
 dotenv.config();
 
+function generateMerkleRoots() {
+  const merkleHelper = useMerkleHelper();
+
+  // checks if both arrays are empty, throw exception to stop smart contract deployment
+  if (ALPHA_SALE_WHITELIST.length === 0 || ADVISORY_WHITELIST.length === 0) {
+    console.error(
+      "EMPTY_LEAVES: Either the whitelist leaves or the advisory leaves is empty."
+    );
+    process.exit(1);
+  }
+
+  // merkle trees
+  const whitelistMerkleTree =
+    merkleHelper.createMerkleTree(ALPHA_SALE_WHITELIST);
+  const advisorMerkleTree = merkleHelper.createMerkleTree(ADVISORY_WHITELIST);
+
+  // merkle roots
+  const whitelistMerkleRoot =
+    merkleHelper.createMerkleRoot(whitelistMerkleTree);
+  const advisorMerkleRoot = merkleHelper.createMerkleRoot(advisorMerkleTree);
+
+  console.log(
+    "Merkle root generated for Alpha Sale whitelist\n",
+    whitelistMerkleRoot
+  );
+  console.log(
+    "Merkle root generated for advisory whitelist\n\n",
+    advisorMerkleRoot
+  );
+
+  return {
+    whitelistMerkleRoot,
+    advisorMerkleRoot,
+  };
+}
+
 const isNetworkPolygonMainnet =
   hre.network.name === "polygon" || hre.network.config.chainId === 137;
 // Just toggle this to `false` Polygon Testnet Mumbai
@@ -208,42 +244,6 @@ async function main() {
   ]);
 
   return process.exit(0);
-}
-
-function generateMerkleRoots() {
-  const merkleHelper = useMerkleHelper();
-
-  // checks if both arrays are empty, throw exception to stop smart contract deployment
-  if (ALPHA_SALE_WHITELIST.length === 0 || ADVISORY_WHITELIST.length === 0) {
-    console.error(
-      "EMPTY_LEAVES: Either the whitelist leaves or the advisory leaves is empty."
-    );
-    process.exit(1);
-  }
-
-  // merkle trees
-  const whitelistMerkleTree =
-    merkleHelper.createMerkleTree(ALPHA_SALE_WHITELIST);
-  const advisorMerkleTree = merkleHelper.createMerkleTree(ADVISORY_WHITELIST);
-
-  // merkle roots
-  const whitelistMerkleRoot =
-    merkleHelper.createMerkleRoot(whitelistMerkleTree);
-  const advisorMerkleRoot = merkleHelper.createMerkleRoot(advisorMerkleTree);
-
-  console.log(
-    "Merkle root generated for Alpha Sale whitelist\n",
-    whitelistMerkleRoot
-  );
-  console.log(
-    "Merkle root generated for advisory whitelist\n\n",
-    advisorMerkleRoot
-  );
-
-  return {
-    whitelistMerkleRoot,
-    advisorMerkleRoot,
-  };
 }
 
 // We recommend this pattern to be able to use async/await everywhere
